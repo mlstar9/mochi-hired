46851857d6c2698481bf6e6fb6a1fc133197230c
Ke Wang <ke@pika.art>
2026-02-09 20:40:13 +0000

    test auto-sync

--- src/Blank.tsx
+++ src/Blank.tsx
@@ -2,8 +2,10 @@ import {AbsoluteFill} from 'remotion';
 
 export const Blank: React.FC = () => {
   return (
-    <AbsoluteFill style={{backgroundColor: '#111111'}}>
-      {/* your content here */}
+    <AbsoluteFill style={{backgroundColor: '#111111', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      <div style={{color: 'white', fontSize: 48, fontFamily: 'sans-serif'}}>
+        it works! 🎉
+      </div>
     </AbsoluteFill>
   );
 };
